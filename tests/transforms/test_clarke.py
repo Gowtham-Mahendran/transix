effc8ffd0406c6fbfe690ab64addeb57812c371e
Gowtham Mahendran <197329014+Gowtham-Mahendran@users.noreply.github.com>
2026-03-02 17:42:43 +0100

    test(clarke): Test cases for inverse clarke transformation tx.ab0_to_abc()
    closes #20

--- tests/transforms/test_clarke.py
+++ tests/transforms/test_clarke.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 import transix as tx
@@ -53,3 +54,32 @@ def test_clarke_power_variant_and_invariant():
     # z     = np.round(z, 4)
 
     # print(alpha,beta,z)
+
+
+def test_clarke_inverse_balanced_three_phase_signal():
+
+    a,b,c = tx.generate_abc(rms=230,f=50,t=0.1,fs=50000)
+
+    # power-invariant
+    alpha,beta,z = tx.abc_to_ab0(a,b,c)
+
+    a1,b1,c1 = tx.ab0_to_abc(alpha,beta,z)
+
+    assert np.allclose(a,a1) and np.allclose(b,b1) and np.allclose(c,c1)
+
+
+    # power-variant
+    alpha,beta,z = tx.abc_to_ab0(a,b,c,variant="power_variant")
+
+    a1,b1,c1 = tx.ab0_to_abc(alpha,beta,z,variant="power_variant")
+
+    assert np.allclose(a,a1) and np.allclose(b,b1) and np.allclose(c,c1)
+
+
+def test_inverse_clarke_wrong_input_variant():
+
+    a,b,c = tx.generate_abc(rms=230,f=50,t=0.1,fs=50000)
+
+    with pytest.raises(ValueError, match=
+    "variant must be 'power_invariant' or 'power_variant'"):
+        tx.ab0_to_abc(a,b,c,variant="wrong_variant")
